dd1bd053d6c13e5bfdf76352c4f406ba8ed641a3
Anonymous1212144 <101783700+Anonymous1212144@users.noreply.github.com>
2024-05-30 18:58:33 -0600

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -10,7 +10,7 @@ function HTMLActuator() {
 var lastRan = Date.now();
 
 HTMLActuator.prototype.actuate = function (grid, metadata) {
-  if ((Date.now() - lastRan) < 2) {
+  if ((Date.now() - lastRan) < 10) {
     return;
   }
   lastRan = Date.now();
